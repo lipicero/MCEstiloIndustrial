ca70bfbbe2a523b71bc4d62277e0710cc51f8cbd
Matías Cero <matiascerolenii@gmail.com>
2025-10-21 20:58:40 -0300

    Feat: añadir soporte para análisis de Vercel y eliminar comentarios innecesarios en index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Analytics } from "@vercel/analytics/react"
 import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import { ThemeProvider } from './contexts/ThemeContext';
@@ -10,11 +11,8 @@ root.render(
     <HelmetProvider>
       <ThemeProvider>
         <App />
+        <Analytics />
       </ThemeProvider>
     </HelmetProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
