cc4cbdcdd6f58cbd0f7aa9a0c8cf36114337760f
Lipi <matiascerolenii@gmail.com>
2025-11-18 21:56:13 -0300

    feat: agregar almacenamiento de sesiones en MySQL y enlace al sitio web en la navegación

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,7 @@ var path = require('path');
 var hbs = require('hbs');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
+var MySQLStore = require('express-mysql-session')(session);
 var fileUpload = require('express-fileupload');
 var cors = require('cors');
 var logger = require('morgan');
@@ -38,9 +39,30 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+// MySQL session store options
+var sessionStoreOptions = {
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB_NAME,
+  checkExpirationInterval: 900000, // How frequently expired sessions will be cleared; milliseconds.
+  expiration: 86400000, // The maximum age of a valid session; milliseconds.
+  createDatabaseTable: true, // Whether or not to create the sessions database table, if one does not already exist.
+  schema: {
+    tableName: 'sessions',
+    columnNames: {
+      session_id: 'session_id',
+      expires: 'expires',
+      data: 'data'
+    }
+  }
+};
+var sessionStore = new MySQLStore(sessionStoreOptions);
 // session middleware
 app.use(session({
   secret: process.env.SESSION_SECRET || 'mi-secreto-local',
+  store: sessionStore,
   resave: false,
   saveUninitialized: false,
   cookie: { maxAge: 24 * 60 * 60 * 1000 } // 1 day
